568f9e8a82c7bf58cbca477aefa33a1076d9c854
coltenappleby <colten.appleby@gmail.com>
2021-08-25 16:32:20 -0400

    user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+
+    def show
+        user = User.find(params[:id])
+    end
+
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            render json: user
+        else
+            render json: user.errors.fulle_messages            
+        end
+    end
+
+
+    private
+
+    def user_params
+        params.permit(:name, :email, :phone_number)
+    end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :phone_number
+
+  has_many :lists
+  has_many :items
 end
